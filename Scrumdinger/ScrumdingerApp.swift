0ac9f68867df6740c2ae6c2d64f8b4e2d1c300d1
Cesar Montelongo <cidhighwind85@hotmail.com>
2022-07-30 15:42:56 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Arc on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
